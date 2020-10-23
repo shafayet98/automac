a2850ee95d652bb8ee24db4051ec4451a45b3b50
shafayet98 <shafayet.islam.61@gmail.com>
2020-10-23 18:14:42 +0600

    speak and ask...

--- mergeAP.py
+++ mergeAP.py
@@ -4,6 +4,10 @@ import os, sys
 from subprocess import Popen, PIPE
 import subprocess
 
+import playsound
+import speech_recognition as sr
+from gtts import gTTS
+
 (current_process, tError) = Popen(['osascript', 'getprocess.applescript'], stdout=PIPE).communicate()
 # print(current_process)
 
@@ -31,10 +35,40 @@ def kill_the_process(kill_process_name):
     else:
         print(out)
 
+def speak(text):
+    tts = gTTS(text = text, lang = "en")
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
 
 
-current_apps = handle_incoming_current_process(current_process)
-print("Your running apps are: ")
-print(current_apps)
-kill_process_name = input("Name the process you want to kill:  ") 
-kill_the_process(kill_process_name)
+def ask():
+    r = sr.Recognizer()
+    mic = sr.Microphone() 
+    with mic as source:
+        print("mic on")
+        audio = r.record(source=mic, duration=10)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Sorry could not recognize your voice")
+    return said
+
+
+def ask_again():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.record(source, duration=10)
+        text = r.recognize_google(audio, language = 'en-IN', show_all = True )
+        print("I thinks you said '" + r.recognize_google(audio) + "'")
+# current_apps = handle_incoming_current_process(current_process)
+# print("Your running apps are: ")
+# print(current_apps)
+# kill_process_name = input("Name the process you want to kill:  ") 
+# kill_the_process(kill_process_name)
+
+speak("Hello How are you")
+ask_again()
